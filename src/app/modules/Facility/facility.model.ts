7d7a8a9ee02989aef84077e56f74c021f4edcf91
rabby-hasan <rabby.65301@gmail.com>
2024-06-13 12:51:08 +0600

    added facility type and model

---
+++ src/app/modules/Facility/facility.model.ts
@@ -0,0 +1,30 @@
+import { Schema, model } from "mongoose";
+import { TFacility } from "./facility.interface";
+
+
+
+const FacilitySchema = new Schema<TFacility>({
+    name: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    pricePerHour: {
+        type: Number,
+        required: true
+    },
+    location: {
+        type: String,
+        required: true
+    },
+    isDeleted: {
+        type: Boolean,
+        required: true
+    }
+})
+
+
+export const Facility = model<TFacility>('facility', FacilitySchema);
